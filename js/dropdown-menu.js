9ec4ee82f2ba078c69310ee70e2020c285fb2346
Michał Stodolny <mich.studiukb@gmail.com>
2017-08-10 23:12:02 +0200

    wstep do desktop

--- js/dropdown-menu.js
+++ js/dropdown-menu.js
@@ -2,7 +2,6 @@ $( document ).ready(function() {
   
     var $btn = jQuery('.btn-menu:eq(0)');
     var $nav = jQuery('nav:eq(0)');
-    $nav.css('display','none');
     
     $btn.click(function(){
        $nav.slideToggle(); 
