8da48a8da55ce63bfc1341ad7246c2ddf6f61164
Michał Stodolny <mich.studiukb@gmail.com>
2017-08-11 19:11:50 +0200

    desktop ready + menu

--- js/dropdown-menu.js
+++ js/dropdown-menu.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
   
     var $btn = jQuery('.btn-menu:eq(0)');
-    var $nav = jQuery('nav:eq(0)');
+    var $nav = jQuery('.nav:eq(0)');
     
     $btn.click(function(){
        $nav.slideToggle(); 
